354c57b9a1e23509ded9ec2b36fc6921fe5eba00
Sergii Bartytskyi <s.bartycjkyj@gmail.com>
2025-06-25 16:05:59 +0200

    add post, put, patch, delete requests

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,5 +1,16 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { TaskService } from './task.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 
 @Controller('task')
 export class TaskController {
@@ -12,6 +23,26 @@ export class TaskController {
 
   @Get('/:id')
   findById(@Param('id') id: string) {
-    return this.taskService.findById(Number(id));
+    return this.taskService.findById(Number(+id));
+  }
+
+  @Post()
+  create(@Body() dto: CreateTaskDto) {
+    return this.taskService.create(dto);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() dto: UpdateTaskDto) {
+    return this.taskService.update(+id, dto);
+  }
+
+  @Patch(':id')
+  patchUpdate(@Param('id') id: string, @Body() dto: Partial<UpdateTaskDto>) {
+    return this.taskService.patchUpdate(+id, dto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.taskService.delete(+id);
   }
 }
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 
 @Injectable()
 export class TaskService {
@@ -25,6 +27,47 @@ export class TaskService {
     if (!task) {
       throw new NotFoundException('Task not found');
     }
+
+    return task;
+  }
+
+  create(dto: CreateTaskDto) {
+    const { title } = dto;
+
+    const newTask = {
+      id: this.tasks.length + 1,
+      title,
+      isCompleted: false,
+    };
+
+    this.tasks.push(newTask);
+
+    return this.tasks;
+  }
+
+  update(id: number, dto: UpdateTaskDto) {
+    const { title, isCompleted } = dto;
+
+    const task = this.findById(id);
+
+    task.title = title;
+    task.isCompleted = isCompleted;
+
+    return task;
+  }
+
+  patchUpdate(id: number, dto: Partial<UpdateTaskDto>) {
+    const task = this.findById(id);
+
+    Object.assign(task, dto);
+
+    return task;
+  }
+
+  delete(id: number) {
+    const task = this.findById(id);
+
+    this.tasks = this.tasks.filter((t) => t.id !== task.id);
 
     return task;
   }
